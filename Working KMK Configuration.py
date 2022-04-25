1285f62a25382f79bac87ff3e608847364524ac0
scndthe2nd <15825472+scndthe2nd@users.noreply.github.com>
2022-04-25 17:11:39 -0400

    Updated keymapping

--- Working KMK Configuration.py
+++ Working KMK Configuration.py
@@ -10,7 +10,9 @@ import neopixel
 from kmk.handlers.sequences import send_string
 from kmk.modules.tapdance import TapDance
 from kmk.scanners import DiodeOrientation
- 
+from kmk.modules.combos import Chord, Combos
+from kmk.modules.oneshot import OneShot
+
 class _KMKKeyboard(KMKKeyboard):
     def __init__(self):
         # create and register the scanner
@@ -19,25 +21,44 @@ class _KMKKeyboard(KMKKeyboard):
             clock=board.D3,
             data=board.D4,
             latch=board.D2,
-            key_count=24,
+            key_count=48,
             # optional arguments with defaults:
             value_to_latch=True, # 74HC165: True, CD4021: False
             value_when_pressed=False,
             interval=0.02,
             max_events=64
         )
+
+## DEF BARE KEYBOARD
 keyboard = _KMKKeyboard()
+
+## MODULES
+### TAPDANCE
 tapdance = TapDance()
 tapdance.tap_time = 750
 keyboard.modules.append(tapdance)
+
+### ONESHOT
+oneshot = OneShot()
+keyboard.modules.append(oneshot)
+
+### COMBOS
+combos = Combos()
+keyboard.modules.append(combos)
+
+### NEOPIXEL INIT
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+color = (0,0,0)
+pixel.brightness = 1
+pixel.fill(color)
  
+## TAPDANCE MACROS
 MACRO1 = KC.TD(
     KC.N1,  # Tap once for "a"
     KC.N2,  # Tap twice for "b"
     # Tap three times to send a raw string via macro
     send_string('macros in a tap dance? I think yes'),
 )
-
 MACRO2 = KC.TD(
     KC.N1,  # Tap once for "a"
     KC.N2,  # Tap twice for "b"
@@ -50,78 +71,48 @@ MACRO3 = KC.TD(
     # Tap three times to send a raw string via macro
     send_string('macros in a tap dance? I think yes'),
 )
-MACRO4 = KC.TD(
-    KC.N1,  # Tap once for "a"
-    KC.N2,  # Tap twice for "b"
-    # Tap three times to send a raw string via macro
-    send_string('macros in a tap dance? I think yes'),
+COMMA_M = KC.TD(
+    KC.COMMA,  # Tap once for ","
+    KC.SEMICOLON,  # Tap twice for ";"
+    KC.COLON, # Tap three times for ":"
 )
-MACRO5 = KC.TD(
-    KC.N1,  # Tap once for "a"
-    KC.N2,  # Tap twice for "b"
-    # Tap three times to send a raw string via macro
-    send_string('macros in a tap dance? I think yes'),
+OBRACKET_M = KC.TD(
+    KC.LEFT_PAREN,  # Tap once for "("
+    KC.LBRACKET,  # Tap twice for "["
+    KC.LEFT_CURLY_BRACE,
+    KC.LEFT_ANGLE_BRACKET,
 )
-MACRO6 = KC.TD(
-    KC.N1,  # Tap once for "a"
-    KC.N2,  # Tap twice for "b"
-    # Tap three times to send a raw string via macro
-    send_string('macros in a tap dance? I think yes'),
+CBRACKET_M = KC.TD(
+    KC.RIGHT_PAREN,  # Tap once for "("
+    KC.RBRACKET,  # Tap twice for "["
+    KC.RIGHT_CURLY_BRACE,
+    KC.RIGHT_ANGLE_BRACKET,
+)
+QUOTE_M = KC.TD(
+    KC.QUOTE,  # Tap once for "'"
+    KC.GRAVE,  # Tap twice for "`"
+    KC.TILDE,  # Tap three times for "~"
+)
+CTRL_MACRO = KC.TD(
+    KC.OS(KC.LCTL,tap_time=None),  # Tap once for "LCTL"
+    KC.OS(KC.LALT,tap_time=None),  # Tap twice for "LALT"
+    KC.OS(KC.LWIN,tap_time=None),  # Tap three times for "LWIN"
 )
 
 keyboard.keymap = [
-    [KC.BACKSPACE,KC.SPACE,KC.ENTER,KC.B,KC.V,KC.Z,KC.LSHIFT,KC.TAB,
+## NORMAL KEYMAP
+    [KC.BACKSPACE,KC.SPACE,KC.ENTER,KC.B,KC.V,KC.Z,KC.LSHIFT,CTRL_MACRO,
     KC.F,KC.C,KC.X,KC.A,KC.S,KC.Q,KC.TAB,KC.G,
     KC.R,KC.D,KC.E,KC.W,KC.N1,KC.N5,KC.N6,KC.T,
-        ],
+
+    KC.LALT,KC.LCTL,KC.LWIN,KC.N,KC.M,KC.SLASH,KC.RSHIFT,KC.ENTER,
+    KC.J,COMMA_M, KC.DOT, QUOTE_M, KC.L, OBRACKET_M, CBRACKET_M, KC.H,
+    KC.U, KC.K, KC.O, KC.P, KC.SCROLL_LOCK, KC.PRINT_SCREEN, KC.PAUSE, KC.Y,
+            ],
 ]
+print("Started")
+keyboard.debug_enabled = False
 
-
-'''
-##Keymap L0
-[
-MACRO1, MACRO2, MACRO3,                                     KC.PS, MACRO6, KC.ESC,
-KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T,           KC.Y, KC.U, KC.I, KC.O, KC.P, KC.BackSlash,
-MACRO4, KC.A, KC.S, KC.D,KC.F,KC.G,             KC.H, KC.J, KC.K, KC.L, KC.Colon, KC.Enter,
-KC.SHIFT,KC.Z,KC.X,KC.C,KC.V,KC.B,              KC.N, KC.M, KC.Comma, KC.Period, KC.Slash, KC.SHIFT
-                KC.BACKSPACE, KC.SPACE, MACRO5  KC.Meta, KC.LCtl, KC.Alt
-]
-
-## Keymap L1
-[
-MACRO1, MACRO2, MACRO3,                                                 KC.PS, MACRO6, KC.ESC,
-KC.TAB, -----, KC.N7, KC.N8, KC.N9, KC.PLUS,    KC.PGUP, KC.HOME, KC.INS, KC.OP, KC.CP, -----,
-MACRO4, -----, KC.N4, KC.N5,KC.N6,KC.MINUS,     KC.PGDN, KC.END, KC.DEL, -----, -----, KC.Enter,
-KC.SHIFT, -----,KC.N1,KC.N2,KC.N3,KC.DOT,       -----, -----, -----, -----, -----, KC.SHIFT,
-            KC.BACKSPACE, KC.SPACE, MACRO5      KC.META, KC.LCTL, KC.ALT
-]
-
-]
-
-
-### Register Keymap
-[
-21  22  23                              46  45  44
-15  14  20  19  17  16      39  40  42  43  37  38
-0   12  13  18  9   8       31  32  41  36  35  23
-7   6   11  10  5   4       27  28  33  34  29  30
-            1   2   3       26  25  24          
-]
-'''
-
-
-
-
-
-keyboard.debug_enabled = True
- 
-pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
- 
-pixel.brightness = 0.01
- 
- 
 if __name__ == '__main__':
     keyboard.go()
-
-while True:
-    pixel.fill((1,1,1))
+    pixel.fill(color)
